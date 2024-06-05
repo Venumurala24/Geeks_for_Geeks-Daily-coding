7e8b485ba7462b7090ff72c1a1a252320cd05b19
Venu_murala <154655347+Venumurala24@users.noreply.github.com>
2024-06-05 14:00:52 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Max Min/max-min.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findSum(self,a,N): 
+        
+        return max(a)+ min(a)
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+t=int(input())
+for _ in range(0,t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    ob = Solution()
+    print(ob.findSum(a,n))
+# } Driver Code Ends
