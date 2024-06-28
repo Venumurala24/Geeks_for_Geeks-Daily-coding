96a88961c85fdd3c7ef000c61f35f774a36ac9ae
Venu_murala <154655347+Venumurala24@users.noreply.github.com>
2024-06-28 22:28:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Does array represent Heap/does-array-represent-heap.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def isMaxHeap(self,arr,n):
+        # Your code goes here            
+        for i in range(n//2):
+            if 2*i+1<n and arr[i]<arr[2*i+1] or 2*i+2<n and arr[i]<arr[2*i+2]:
+                return False
+        return True
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__ =='__main__':
+    t= int(input())
+    for tcs in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().split()]
+        ob=Solution()
+        print(int(ob.isMaxHeap(arr,n)))
+# } Driver Code Ends
